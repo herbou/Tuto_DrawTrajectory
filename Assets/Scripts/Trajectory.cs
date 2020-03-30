7914e4d8a2ccfcb5767f9d560c05868bc4f15698
hamza herbou <35342314+herbou@users.noreply.github.com>
2020-03-30 18:20:33 +0100

    Update Trajectory.cs

--- Assets/Scripts/Trajectory.cs
+++ Assets/Scripts/Trajectory.cs
@@ -50,6 +50,11 @@ public class Trajectory : MonoBehaviour
 			pos.x = (ballPos.x + forceApplied.x * timeStamp);
 			pos.y = (ballPos.y + forceApplied.y * timeStamp) - (Physics2D.gravity.magnitude * timeStamp * timeStamp) / 2f;
 		
+			//you can simlify this 2 lines at the top by:
+//pos = (ballPos+force*time)-((-Physics2D.gravity*time*time)/2f);
+//
+//but make sure to turn "pos" in Ball.cs to Vector2 instead of Vector3	
+			
 			dotsList [i].position = pos;
 			timeStamp += dotSpacing;
 		}
